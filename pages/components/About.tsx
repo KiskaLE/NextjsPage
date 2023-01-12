75e277c98224fe7f9b5ca3a11fd28df370b3ffa4
KiskaLE <vojtakylar@seznam.cz>
2023-01-12 20:49:39 +0100

    added Hero section

---
+++ pages/components/About.tsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-unescaped-entities */
+import Transition from "./Transition";
+
+export default function About() {
+  return (
+    <>
+      <section
+        id="about"
+        className="text-white font-bakbak-one flex flex-col items-center py-5 min-h-screen"
+      >
+        <Transition>
+          <h1 className="text-center uppercase font-bakbak-one font-bold text-yellow-400 text-3xl mb-5">
+            About
+          </h1>
+          <p className="text-justify max-w-[500px]">
+            I'm studying at the{" "}
+            <a className="text-yellow-400" href="https://www.spse.cz/">
+              SPŠE & VOŠ in Pardubice
+            </a>{" "}
+            with specialization on <strong>IT</strong>. I enjoy programming
+            applications in <strong>Java</strong> and
+            <strong> Next.js</strong>. Right now I'm learning to create
+            applications in React with Tailwind. After school I would like to
+            become a programmer
+          </p>
+        </Transition>
+      </section>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
-/* eslint-disable react/no-unescaped-entities */
 import Head from "next/head";
-import { Inter } from "@next/font/google";
 import Hero from "./components/Hero";
-import Transition from "./components/Transition";
+import About from "./components/About";
 
 export default function Home() {
   return (
@@ -16,27 +14,7 @@ export default function Home() {
       <div className=" -z-50 fixed top-0 bg-warp-loop bg-cover bg-center h-screen w-screen"></div>
       <main className="px-10 mt-[64px]">
         <Hero />
-        <section
-          id="about"
-          className="text-white font-bakbak-one flex flex-col items-center py-5 min-h-screen"
-        >
-          <Transition>
-            <h1 className="text-center uppercase font-bakbak-one font-bold text-yellow-400 text-3xl mb-5">
-              About
-            </h1>
-            <p className="text-justify max-w-[500px]">
-              I'm studying at the{" "}
-              <a className="text-yellow-400" href="https://www.spse.cz/">
-                SPŠE & VOŠ in Pardubice
-              </a>{" "}
-              with specialization on <strong>IT</strong>. I enjoy programming
-              applications in <strong>Java</strong> and
-              <strong> Next.js</strong>. Right now I'm learning to create
-              applications in React with Tailwind. After school I would like to
-              become a programmer
-            </p>
-          </Transition>
-        </section>
+        <About />
       </main>
     </>
   );
